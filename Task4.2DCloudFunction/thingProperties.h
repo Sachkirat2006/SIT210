d8bfccfd373dccfdddad4567c20fec669fda7288
Sachkirat2006 <sach15102006@gmail.com>
2026-03-19 19:13:25 -0700

    Create thingProperties.h

---
+++ Task4.2DCloudFunction/thingProperties.h
@@ -0,0 +1,35 @@
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <Arduino_NetworkConfigurator.h>
+#include "configuratorAgents/agents/BLEAgent.h"
+#include "configuratorAgents/agents/SerialAgent.h"
+void onRoomCommandChange();
+void onBathroomStateChange();
+void onClosetStateChange();
+void onLivingRoomStateChange();
+
+String roomCommand;
+bool bathroomState;
+bool closetState;
+bool livingRoomState;
+
+KVStore kvStore;
+BLEAgentClass BLEAgent;
+SerialAgentClass SerialAgent;
+WiFiConnectionHandler ArduinoIoTPreferredConnection; 
+NetworkConfiguratorClass NetworkConfigurator(ArduinoIoTPreferredConnection);
+
+void initProperties(){
+  NetworkConfigurator.addAgent(BLEAgent);
+  NetworkConfigurator.addAgent(SerialAgent);
+  NetworkConfigurator.setStorage(kvStore);
+  // For changing the default reset pin uncomment and set your preferred pin. Use DISABLE_PIN for disabling the reset procedure.
+  //NetworkConfigurator.setReconfigurePin(your_pin);
+  ArduinoCloud.setConfigurator(NetworkConfigurator);
+
+  ArduinoCloud.addProperty(roomCommand, READWRITE, ON_CHANGE, onRoomCommandChange);
+  ArduinoCloud.addProperty(bathroomState, READWRITE, ON_CHANGE, onBathroomStateChange);
+  ArduinoCloud.addProperty(closetState, READWRITE, ON_CHANGE, onClosetStateChange);
+  ArduinoCloud.addProperty(livingRoomState, READWRITE, ON_CHANGE, onLivingRoomStateChange);
+
+}
